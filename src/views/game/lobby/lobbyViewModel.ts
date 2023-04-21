e01372de26f6e92cbfa5d7e544fd53bee62ae31e
Mael <legrand.mael2002@gmail.com>
2023-04-21 13:22:28 +0200

    hot fix

--- src/views/game/lobby/lobbyViewModel.ts
+++ src/views/game/lobby/lobbyViewModel.ts
@@ -1,11 +1,26 @@
 import {useLocation, useNavigate} from "react-router-dom";
 import {useAuth} from "../../../components/auth/AuthContext";
 import {User} from "../../../domain/user/user.domain";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import jwtDecode from "jwt-decode";
 
 export const lobbyViewModel = () => {
+    const [user, setUser] = useState<User | null>(null);
+    const {token} = useAuth();
 
-    const { user } = useAuth();
+    const getUserData = async () => {
+        const decodedToken: User = jwtDecode(token || '');
+        const email = decodedToken.email;
+        const data = await axios.get(`http://localhost:3000/users/me/${email}`)
+        return data.data;
+    }
+
+    useEffect(() => {
+        getUserData().then((data) => {
+            setUser(data);
+        });
+    }, [])
 
     const { search } = useLocation()
     const navigate = useNavigate()
@@ -13,17 +28,19 @@ export const lobbyViewModel = () => {
     const isPublic = JSON.parse(urlParams.get('p') as string)
 
     useEffect(() => {
-        if (!user) {
+        if (!token) {
             navigate('/login')
         }
-    }, [user])
+    }, [token])
 
     const returnHome = () => {
         navigate('/')
     }
 
     const users: User[] = []
-    users.push(user as User)
+    if (user instanceof User) {
+        users.push(user)
+    }
 
     return {
         isPublic,
